2707abb0b934e173abdc73453c6ec325adba8910
emaldie <trueyamuxa223@gmail.com>
2024-11-29 18:24:56 +0300

    seat & room entities, config in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,12 @@ import { OrdersModule } from "./orders/orders.module";
 import { FilmsModule } from "./films/films.module";
 import * as Joi from "joi";
 import { FilmEntity } from "./films/entities/film.entity";
-import { AuthModule } from "./auth/auth.module";
+import { AuthModule } from './auth/auth.module';
+import { SeatsModule } from './seats/seats.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { RoomEntity } from "./rooms/entities/room.entity";
+import { SeatEntity } from "./seats/entities/seat.entity";
+
 
 @Module({
     imports: [
@@ -31,16 +36,18 @@ import { AuthModule } from "./auth/auth.module";
         TypeOrmModule.forRootAsync({
             useFactory: (configService: ConfigService) => ({
                 type: "postgres",
-                host: "localhost",
-                port: 5432,
-                username: "postgres",
+                host: configService.getOrThrow<string>("DB_HOST"),
+                port: +configService.getOrThrow<string>("DB_PORT"),
+                username: configService.getOrThrow<string>("DB_USERNAME"),
                 password: configService.getOrThrow<string>("DB_PASSWORD"),
                 database: configService.getOrThrow<string>("DB_NAME"),
-                entities: [UserEntity, FilmEntity],
+                entities: [UserEntity, FilmEntity, RoomEntity, SeatEntity],
                 synchronize: true
             }),
             inject: [ConfigService]
-        })
+        }),
+        SeatsModule,
+        RoomsModule,
     ],
     controllers: [AppController],
     providers: [AppService]
---
+++ src/seats/dto/seat.dto.ts
@@ -0,0 +1,7 @@
+import { SeatEntity } from "../entities/seat.entity";
+import { RoomDto } from '../../rooms/dto/room.dto';
+
+export class SeatDto extends SeatEntity {
+    id: number;
+    room: RoomDto;
+}
